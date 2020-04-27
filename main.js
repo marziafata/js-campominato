75a9f378295b12ba9c9cec5a60c5cfb9f98cf3d3
Marzia Fatarella <marziafatarella@gmail.com>
2020-04-27 16:29:07 +0200

    disastro

--- main.js
+++ main.js
@@ -7,3 +7,31 @@
 // con difficoltà 0=> si gioca con numeri che vanno da 1 a 100
 // con difficoltà 1 => si gioca con numeri che vanno da 1 a 80
 // con difficoltà 2=> si gioca con numeri che vanno da 1 a 50
+
+//creo una funzione che generi i numeri causali da 1 a 100
+function genera_random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+// attraverso un ciclo for, genero 16 numeri casuali, sfruttando la funzione genera_random
+for (var i = 0; i < 16; i++) {
+    var mine = genera_random(1, 100);
+    console.log('Numeri mina: ' + mine);
+}
+
+// ok ho ottenuto le mie mine.
+// adesso devo fare in modo che l'utente scelga i suoi numeri con questi criteri
+// tra 1 e 100
+// massimo 84 giocate
+// deve inserirli uno alla volta
+// per fare questo ho bisogno di un ciclo do while perchè sono sicura che almeno una volta il numero glielo devo chiedere.
+
+do {
+    var numero = parseInt(prompt('inserisci un numero tra 1 e 100'));
+    console.log(numero);
+    if (numero != mine) {
+        alert('sei salvo! gioca di nuovo');
+    } else {
+        alert('boom!')
+    }
+} while();
