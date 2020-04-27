f347b9156179f1ff2afeb6af93a63d8733a6a219
Marzia Fatarella <marziafatarella@gmail.com>
2020-04-27 16:52:37 +0200

    atri tentativi

--- main.js
+++ main.js
@@ -13,11 +13,26 @@ function genera_random(min, max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
+var numero = parseInt(prompt('inserisci un numero tra 1 e 100'));
+console.log(numero);
+
 // attraverso un ciclo for, genero 16 numeri casuali, sfruttando la funzione genera_random
-for (var i = 0; i < 16; i++) {
-    var mine = genera_random(1, 100);
-    console.log('Numeri mina: ' + mine);
+function genera_mine(x) {
+    for (var i = 0; i < 16; i++) {
+        var mine = genera_random(1, 100);
+        console.log('Numeri mina: ' + mine);
+        if (numero == mine[i]) {
+            console.log("boom");
+            return "boom"
+        } else {
+            console.log("ok");
+            return "ok"
+        }
+    }
 }
+
+
+
 
 // ok ho ottenuto le mie mine.
 // adesso devo fare in modo che l'utente scelga i suoi numeri con questi criteri
@@ -26,12 +41,10 @@ for (var i = 0; i < 16; i++) {
 // deve inserirli uno alla volta
 // per fare questo ho bisogno di un ciclo do while perchè sono sicura che almeno una volta il numero glielo devo chiedere.
 
-do {
-    var numero = parseInt(prompt('inserisci un numero tra 1 e 100'));
-    console.log(numero);
-    if (numero != mine) {
-        alert('sei salvo! gioca di nuovo');
-    } else {
-        alert('boom!')
-    }
-} while();
+// var numero = parseInt(prompt('inserisci un numero tra 1 e 100'));
+// console.log(numero);
+// if (numero != mine) {
+//     alert('sei salvo! gioca di nuovo');
+// } else {
+//     alert('boom!')
+// }
